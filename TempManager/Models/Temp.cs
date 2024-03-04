f3f87bbfe1c76690a12d686fb6a92963a238b4a0
Logan Miller <logan.miller@southeasttech.edu>
2024-03-03 20:19:37 -0600

    Add data validation to the Temp domain model.

--- TempManager/Models/Temp.cs
+++ TempManager/Models/Temp.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TempManager.Models
 {
     public class Temp
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
         public DateTime? Date { get; set; }
+
+        [Required(ErrorMessage = "Low temperature is required")]
+        [Range(-200, 200, ErrorMessage = "Low temperature must be between -200 and 200")]
         public double? Low { get; set; }
+
+        [Required(ErrorMessage = "High temperature is required")]
+        [Range(-200, 200, ErrorMessage = "High temperature must be between -200 and 200")]
         public double? High { get; set; }
     }
 }
